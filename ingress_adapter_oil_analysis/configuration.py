2cf47bd4482e1440c4a2ed4542b770ca7cf4bbfc
Nasib Sarvari <nasibo2005@hotmail.com>
2021-04-29 14:35:21 +0200

    3 fields added and code refactored

---
+++ ingress_adapter_oil_analysis/configuration.py
@@ -0,0 +1,33 @@
+"""
+Contains the configuration.
+"""
+import configparser
+from configparser import ConfigParser
+import logging.config
+from logging import Logger
+
+
+class Configuration:
+    """
+    Contains methods to obtain configurations for this application.
+    """
+
+    def __init__(self, name: str):
+        self.config = configparser.ConfigParser()
+        self.config.read(['conf.ini', '/etc/osiris/conf.ini', '/etc/transform-ingress2event-time-conf.ini'])
+
+        logging.config.fileConfig(fname=self.config['Logging']['configuration_file'], disable_existing_loggers=False)
+
+        self.name = name
+
+    def get_config(self) -> ConfigParser:
+        """
+        The configuration for the application.
+        """
+        return self.config
+
+    def get_logger(self) -> Logger:
+        """
+        A customized logger.
+        """
+        return logging.getLogger(self.name)
